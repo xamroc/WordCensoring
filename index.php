c1733664f09d8da90442f4d65425c03f4413e029
Marco <merrygoaround@gmail.com>
2013-04-14 01:55:44 -0700

    Second Commit

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 	
+if (isset($_POST['user_input']) && !empty($_POST['user_input'])) {
+	echo 'Works.';
+}
+	
 ?>
+
+<hr>
 
 <form action="index.php" method="POST">
 	<textarea name="user_input" rows="6" cols="30"></textarea><br><br>
